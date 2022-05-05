78b68ed02d6e010e337a2c0ee2a377de8144074b
Ali Ibrahim <aliibra395@gmail.com>
2022-05-05 09:02:35 +0200

    [test] Added Concert test scaffold plan

---
+++ tests/Feature/ConcertsViewListingTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ConcertsViewListingTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function user_can_view_concerts_list()
+    {
+        /**
+         * Setup
+         */
+        // Create concert
+
+
+        /**
+         * Act
+         */
+        // Hit concert show endpoint
+
+
+        /**
+         * Assert
+         */
+    }
+}
